7fd35ec8af6f308d6435e2d6a0c4506faa77e4ef
M-Saffan75 <saffan.khan175@gmail.com>
2024-03-23 18:23:46 -0700

    order update

--- routes/hope_routes.js
+++ routes/hope_routes.js
@@ -8,8 +8,8 @@ const hoperouter = Router()
 
 /* user Private Routes start Here */
 
-hoperouter.use('/create/hope', Valid_User);
 hoperouter.use('/fetch/hope', Valid_User);
+hoperouter.use('/create/hope', Valid_User);
 
 /* user Private Routes End Here */
 
