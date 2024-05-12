0641860f6adb07f06ca37c4bc8977c3ffe89ceab
M-Saffan75 <saffan.khan175@gmail.com>
2024-05-11 21:45:53 -0700

    audio format change

--- controllers/hope_controller.js
+++ controllers/hope_controller.js
@@ -91,7 +91,7 @@ const Remove_hope = async (req, res) => {
         return res.status(200).json({ message: "Hope Successfully Deleted", code: 200 });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: 'Internal Server Error', error: error.message });
+        return res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
 };
 
