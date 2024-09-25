3aef211984d84cbe84a1239378a6b3a5474aab47
M.Saffan Khan <92519898+M-Saffan75@users.noreply.github.com>
2024-09-25 21:43:45 +0500

    Update reflect_controller.js

--- controllers/reflect_controller.js
+++ controllers/reflect_controller.js
@@ -89,7 +89,7 @@ const Remove_reflectful = async (req, res) => {
         return res.status(200).json({ message: "ReflectFul Successfully Deleted", code: 200, status: "success" });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: 'Internal Server Error', error: error.message, status: "failed" });
+        return res.status(500).json({ message: "Internal Server Error", error: error.message, status: "failed" });
     }
 };
 
